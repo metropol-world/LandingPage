bee55b424dfcf7b5a64852b4fd2d564f42ab21b2
Rizq Yazed <rizqyazed@MacBookPro.lan>
2025-08-02 08:59:31 +0100

    added PORT env var

--- BACKEND/src/app.ts
+++ BACKEND/src/app.ts
@@ -2,10 +2,12 @@ import express from "express";
 import emailRouter from "./models/email/email.router";
 
 const app = express();
+const PORT = process.env.PORT || 8080;
+
 app.use(express.json());
 
 app.use("/api/", emailRouter);
 
-app.listen(8080, () => {
-  console.log("App is live!");
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`);
 });
