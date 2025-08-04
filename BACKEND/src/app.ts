7ca5831350d68a5e6fd8ee81904352b956eaab43
Rizq Yazed <rizqyazed@MacBookPro.lan>
2025-08-04 12:08:22 +0100

    changed api route

--- BACKEND/src/app.ts
+++ BACKEND/src/app.ts
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.use("/api/", emailRouter);
+app.use("/api/email", emailRouter);
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
