88c0e5a19d8827c22e941e42793ba967029f3a1e
Rizq Yazed <rizqyazed@Rizqs-MacBook-Pro-8.local>
2025-08-07 15:04:28 +0100

    docker file

--- BACKEND/src/models/email/email.controller.ts
+++ BACKEND/src/models/email/email.controller.ts
@@ -1,10 +1,17 @@
 import { Response, Request } from "express";
 import prisma from "../../utils/prisma";
-import { sendEmailInput } from "./email.schema";
+import sendEmailSchema, { sendEmailInput } from "./email.schema";
 
 export const sendEmail = async (req: Request, res: Response) => {
   try {
     const userData: sendEmailInput = req.body;
+    const parsed = sendEmailSchema.safeParse(userData);
+    if (!parsed.success) {
+      return res.status(500).json({
+        error:
+          "Please enter a valid email/ You have already entered your email",
+      });
+    }
     const user = await prisma.email_submissions.create({
       data: userData,
       omit: {
--- BACKEND/src/models/email/email.schema.ts
+++ BACKEND/src/models/email/email.schema.ts
@@ -1,7 +1,14 @@
 import { z } from "zod";
 
+const strictEmailRegex = /^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$/;
+
 const sendEmailSchema = z.object({
-  email: z.email({ message: "invalid email address" }),
+  email: z
+    .email({ message: "invalid email address" })
+    .refine((email) => strictEmailRegex.test(email), {
+      message: "Stricter format required",
+    }),
 });
 
 export type sendEmailInput = z.infer<typeof sendEmailSchema>;
+export default sendEmailSchema;
